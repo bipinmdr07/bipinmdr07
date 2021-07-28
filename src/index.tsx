75606fc337f0bc5bbb54d902db49bf181810fe73
bipinmdr07 <kubipin07@gmail.com>
2021-07-28 08:25:23 +0545

    Add react-icons to dependencies for icons.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import './public';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
