c0fffb6243391a0bcd67dafe5e56b57539621f24
bipinmdr07 <kubipin07@gmail.com>
2021-09-23 21:17:14 +0545

    Send the experience card as children to timeline component.

--- src/components/common/timeline/Timeline.tsx
+++ src/components/common/timeline/Timeline.tsx
@@ -2,31 +2,14 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 
 import Icon from '../icon';
 
-type Experience = {
-  date: string;
-  company: string;
-  place: string;
-  designation: string;
-  description: string;
-};
-
 interface PropInterface {
-  experiences: Array<Experience>;
+  children?: React.ReactNode;
 }
 
 const Timeline = (props: PropInterface) => (
   <div className="timeline-container">
     <VerticalTimeline>
-      {props.experiences.map((experience, i) => (
-        <VerticalTimelineElement date={experience.date} key={i} className="secondary">
-          <div className="vertical-timeline-element">
-            <h3>{experience.company}</h3>
-            <h4>{experience.place}</h4>
-            <h5>{experience.designation}</h5>
-            <p>{experience.description}</p>
-          </div>
-        </VerticalTimelineElement>
-      ))}
+      {props.children}
       <VerticalTimelineElement className="hidden" icon={<Icon name="star" />}></VerticalTimelineElement>
     </VerticalTimeline>
   </div>
--- src/components/modules/experience/Experience.tsx
+++ src/components/modules/experience/Experience.tsx
@@ -1,10 +1,13 @@
+import { VerticalTimelineElement } from 'react-vertical-timeline-component';
+
 import Timeline from 'components/common/timeline';
+import Sidesheet from 'components/common/sidesheet';
 
 const Experience = () => {
   const experiences = [
     {
       date: '01/2020 - present',
-      company: 'Leapfrog',
+      company: 'Leapfrog Technology',
       place: 'Kathmandu, Nepal',
       designation: 'Software Engineer',
       description:
@@ -12,7 +15,7 @@ const Experience = () => {
     },
     {
       date: '09/2018 - 01/2020',
-      company: 'Leapfrog',
+      company: 'Leapfrog Technology',
       place: 'Kathmandu, Nepal',
       designation: 'Associate Software Engineer',
       description:
@@ -20,7 +23,7 @@ const Experience = () => {
     },
     {
       date: '07/2018 - 09/2018',
-      company: 'Leapfrog',
+      company: 'Leapfrog Technology',
       place: 'Kathmandu, Nepal',
       designation: 'Intern',
       description:
@@ -31,12 +34,30 @@ const Experience = () => {
       company: 'FourMB',
       place: 'Kathmandu, Nepal',
       designation: 'Junior Ruby on Rails Developer',
+      mainSkills: ['ruby on rails', 'postgresql', 'TDD'],
+      skills: ['ruby', 'ruby on rails', 'rspec', 'TDD', 'postgresql'],
       description:
         'I started my career in this Startup company as Web Developer. I was very glad to work here as Junior Ruby on Rails Developer.',
     },
   ];
 
-  return <Timeline experiences={experiences}></Timeline>;
+  return (
+    <>
+      <Timeline>
+        {experiences.map((experience, i) => (
+          <VerticalTimelineElement date={experience.date} key={i} className="secondary">
+            {/* <Card>Body</Card> */}
+            <div className="vertical-timeline-element-hehe">
+              <h3>{experience.company}</h3>
+              <h4>{experience.place}</h4>
+              <h5>{experience.designation}</h5>
+              <p>{experience.description}</p>
+            </div>
+          </VerticalTimelineElement>
+        ))}
+      </Timeline>
+    </>
+  );
 };
 
 export default Experience;
