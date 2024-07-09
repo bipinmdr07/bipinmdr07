e5332ebf44aa57e09c9c19b0014fd1fb7e7b6924
Bipin Manandhar <kubipin07@gmail.com>
2024-07-09 07:53:44 +0545

    Add about page template.

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This is a simple app that uses the{" "}
+        <a href="https://jsonplaceholder.typicode.com/">JSONPlaceholder</a> API
+        to fetch and display data.
+      </p>
+    </div>
+  );
+}
