6b2d75a718e2883e9360c01b024740f02a52be0c
Bipin Manandhar <kubipin07@gmail.com>
2024-07-10 09:15:34 +0545

    Remove meteor from home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import { Vortex } from "@/app/components/ui/vortex";
-import { Meteors } from "@/app/components/ui/meteors";
-import { HeroNav } from "@/app/components/ui/hero-nav";
+import { Vortex } from '@/app/components/ui/vortex';
+import { HeroNav } from '@/app/components/ui/hero-nav';
 
-import { navs } from "@/constant";
+import { navs } from '@/constant';
 
 export default function Home() {
   return (
-    <div className="w-[calc(100%-4rem)] mx-auto rounded-md h-screen overflow-hidden">
+    <div className="mx-auto h-screen w-[calc(100%-4rem)] overflow-hidden rounded-md">
       <Vortex
         backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+        rangeSpeed={9}
+        particleCount={25}
+        className="flex h-full w-full flex-col items-center justify-center px-2 py-4 md:px-10"
       >
         <Image
           src="/avatar.jpg"
@@ -21,11 +22,9 @@ export default function Home() {
           className="rounded-full"
           priority
         />
-        <h2 className="text-white text-xl md:text-6xl font-bold text-center mt-12">
+        <h2 className="mt-12 text-center text-xl font-bold text-white md:text-6xl">
           Bipin Manandhar
         </h2>
-
-        <Meteors number={130} width={1920} />
 
         <HeroNav navs={navs} />
       </Vortex>
