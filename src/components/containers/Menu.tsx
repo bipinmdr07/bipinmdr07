f0c9ad05529d2e45327589d5deae419159a7968d
Bipin Manandhar <kubipin07@gmail.com>
2024-10-07 21:37:50 +0545

    Add active state to nav links.

--- src/components/containers/Menu.tsx
+++ src/components/containers/Menu.tsx
@@ -3,15 +3,22 @@
 import { ROUTES } from "@/utils/data"
 import Link from "next/link"
 
+import { cn } from "@/lib/utils"
+import { usePathname } from "next/navigation"
+
 export default function Menu() {
-  return  <header>
+  const pathname = usePathname()
+
+  return <header>
     <nav className="mb-10">
       <ul>
-        { ROUTES.map((route) => {
-          return <li key={route.id}>
-            <Link href={route.path}>{route.name}</Link>
-          </li>
-        }) }
+        {ROUTES.map((route) => {
+          return (<li key={route.id}>
+            <Link href={route.path}
+              className={cn("hover:text-primary", pathname === route.path ? "text-primary" : '')}
+            >{route.name}</Link>
+          </li>)
+        })}
       </ul>
     </nav>
   </header>
