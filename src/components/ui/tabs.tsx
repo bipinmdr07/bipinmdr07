c070bcbd27348a512a83e91e3fb0490e422feb67
Bipin Manandhar <kubipin07@gmail.com>
2024-10-10 16:42:01 +0545

    Add react-tabs library and add tab components.

---
+++ src/components/ui/tabs.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { forwardRef, Ref } from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+
+import { cn } from '@/lib/utils';
+import { motion } from 'framer-motion';
+
+export const MotionTabs = motion(TabsPrimitive.Root);
+
+export const TabsList = forwardRef<HTMLDivElement, TabsPrimitive.TabsListProps>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn(
+      'inline-flex h-10 items-center justify-center gap-x-2 rounded-md bg-muted p-1 text-muted-foreground',
+      className
+    )}
+    {...props}
+  />
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
+
+export const TabsTrigger = forwardRef<HTMLButtonElement, TabsPrimitive.TabsTriggerProps>(
+  ({ className, ...props }, ref) => (
+    <TabsPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        'inline-flex items-center justify-center whitespace-nowrap rounded-full bg-accent px-5 py-1.5 font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state-active]:font-semibold data-[state=active]:text-primary data-[state=active]:shadow-sm',
+        className
+      )}
+      {...props}
+    />
+  )
+);
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+
+export const TabsContent = forwardRef<HTMLDivElement, TabsPrimitive.TabsContentProps>(
+  ({ className, ...props }, ref) => (
+    <TabsPrimitive.Content
+      ref={ref}
+      className={cn(
+        'ml-1 mt-4 rounded-xl bg-accent p-5 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+        className
+      )}
+      {...props}
+    ></TabsPrimitive.Content>
+  )
+);
+TabsContent.displayName = TabsPrimitive.Content.displayName;
+
+export const MotionTabsContent = motion(TabsContent);
