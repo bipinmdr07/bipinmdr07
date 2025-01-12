3ceabfaf85e52dbb5a15533533d811b69419ae61
Bipin Manandhar <kubipin07@gmail.com>
2025-01-12 19:07:33 +0545

    Add the boiler plate for services page.

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,58 @@
+import { Button } from "@/components/ui/button";
+import { SERVICES } from "@/utils/data";
+import { transition, variants } from "@/utils/framer_variants";
+import { MotionDiv } from "@/utils/motionTags";
+import { Icon } from '@iconify/react'
+
 export default function page() {
-  return <div>Services Page</div>
+  return (
+    <div className="container min-h-screen grid place-items-center pb-7 pt-32">
+      <div>
+        {/* Heading */}
+        <MotionDiv
+          initial="initial"
+          animate="animate"
+          variants={variants.moveUp}
+          transition={transition.moveUp}
+          className="text-center"
+        >
+          <h3>Services <span>Offered</span></h3>
+        </MotionDiv>
+
+        {/* Content */}
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">{
+          SERVICES.map((service, i) => {
+            return (
+              <MotionDiv key={service.id}
+                initial="initial"
+                animate="animate"
+                variants={variants.moveRight}
+              transition={{
+                ...transition.moveRight,
+                delay: i * 0.6
+              }}
+                className="bg-accent p-5 cursor-pointer group rounded-3xl flex flex-col justify-between">
+                <div>
+                  <Icon icon={service.icon} width={55} height={55} className="" />
+                  <div>
+                    <h3 className="mb-2">
+                      <span>
+                        {service.name.split(" ")[0]}
+                      </span>
+                      &nbsp;
+                      {service.name.split(" ").slice(1).join(' ')}
+                    </h3>
+                    <small className="text-accent-foreground">{service.description}</small>
+                  </div>
+                </div>
+                <Button variant="plain" size="sm" className="block ml-auto mt-7">
+                  Learn More
+                </Button>
+              </MotionDiv>
+            )
+          })
+        }</div>
+      </div>
+    </div>
+  )
 }
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -49,3 +49,69 @@ export const SOCIAL_MEDIA = Object.freeze([
     icon: 'mdi:github',
   },
 ]);
+
+// my service details
+export const SERVICES = Object.freeze([
+  {
+    id: 1,
+    name: "UX Design",
+    icon: "mdi:palette",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl."
+  },
+  {
+    id: 2,
+    name: "Web Development",
+    icon: "mdi:web",description: "* Aenean in sem ac leo mollis blandit."
+  },
+  {
+    id: 3,
+    name: "3D Design",
+    icon: "mdi:unity",description: "* Aenean in sem ac leo mollis blandit."
+  }
+])
+
+// expertise skills
+export const EXPERTISE_AREA = Object.freeze([
+  {
+    id: 1,
+    name: 'Web Development',
+    icon: '',
+    description: '',
+    percentage: ''
+  },
+  {
+    id: 2,
+    name: 'API Development',
+    icon: '',
+    description: '',
+    percentage: ''
+  },
+  {
+    id: 3,
+    name: 'Mobile Development',
+    icon: '',
+    description: '',
+    percentage: ''
+  },
+  {
+    id: 4,
+    name: 'UI/UX Design',
+    icon: '',
+    description: '',
+    percentage: ''
+  },
+  {
+    id: 5,
+    name: 'Performance Optimization',
+    icon: '',
+    description: '',
+    percentage: ''
+  },
+  {
+    id: 6,
+    name: 'Unit Testing',
+    icon: '',
+    description: '',
+    percentage: ''
+  },
+])
