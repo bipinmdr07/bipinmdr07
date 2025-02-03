dc8a4455ee10aa4ee7fde670dbf604f5f680beaf
Bipin Manandhar <kubipin07@gmail.com>
2025-02-03 22:39:19 +0545

    Add eslint --fix to pre-commit script

--- src/utils/createMarkup.ts
+++ src/utils/createMarkup.ts
@@ -1,5 +1,5 @@
-import dompurify from 'dompurify'
+import dompurify from 'dompurify';
 
 export const createMarkup = (dirty: string) => {
-  return { __html: dompurify.sanitize(dirty)}
-}
+  return { __html: dompurify.sanitize(dirty) };
+};
