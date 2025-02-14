62f604accb5a895384d082f2bcfbf00350c3b065
Bipin Manandhar <kubipin07@gmail.com>
2025-02-14 20:25:36 +0545

    Fix the type error for motionDiv component.

--- src/components/containers/Mermaid.tsx
+++ src/components/containers/Mermaid.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState, JSX } from 'react';
 import mermaid, { RenderResult } from 'mermaid';
 
 export interface MermaidDiagramProps {
--- src/utils/motionTags.tsx
+++ src/utils/motionTags.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { motion } from 'framer-motion';
+import { motion, MotionProps } from 'framer-motion';
 import Image from 'next/image';
+import { ReactNode, FC } from 'react';
+
+interface MotionDivProps extends MotionProps {
+  children: ReactNode;
+  className?: string;
+  title?: string;
+}
 
 export const MotionImage = motion.create(Image);
 
-export const MotionDiv = motion.div;
+export const MotionDiv: FC<MotionDivProps> = ({ children, ...props }) => <motion.div {...props}>{children}</motion.div>;
 
 export const MotionList = motion.create(motion.li);
 
